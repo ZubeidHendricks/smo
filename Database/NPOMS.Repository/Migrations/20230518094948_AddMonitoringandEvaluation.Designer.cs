6e76b1ed75144d18c15b84b34290c78ce53babfe
Kathiravan Murugan <Kathiravan.Murugan@westerncape.gov.za>
2023-05-18 11:55:13 +0200

    AddMonitoringandEvaluation-Changes

--- Database/NPOMS.Repository/Migrations/20230518094948_AddMonitoringandEvaluation.Designer.cs
+++ Database/NPOMS.Repository/Migrations/20230518094948_AddMonitoringandEvaluation.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NPOMS.Repository;
 
@@ -11,9 +12,10 @@ using NPOMS.Repository;
 namespace NPOMS.Repository.Migrations
 {
     [DbContext(typeof(RepositoryContext))]
-    partial class RepositoryContextModelSnapshot : ModelSnapshot
+    [Migration("20230518094948_AddMonitoringandEvaluation")]
+    partial class AddMonitoringandEvaluation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
