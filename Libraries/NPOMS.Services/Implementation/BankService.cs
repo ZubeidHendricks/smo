b51c51ef86a0cf89724537645e146028a098f79e
Kathiravan Murugan <Kathiravan.Murugan@westerncape.gov.za>
2023-06-13 06:16:51 +0200

    Bank-Detail-Changes

---
+++ Libraries/NPOMS.Services/Implementation/BankService.cs
@@ -0,0 +1,73 @@
+﻿using NPOMS.Domain.Budget;
+using NPOMS.Domain.Entities;
+using NPOMS.Repository;
+using NPOMS.Repository.Implementation.Core;
+using NPOMS.Repository.Interfaces.Budget;
+using NPOMS.Repository.Interfaces.Core;
+using NPOMS.Repository.Interfaces.Entities;
+using NPOMS.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NPOMS.Services.Implementation
+{
+	public class BankService : IBankService
+	{
+		#region Fields
+
+		private IBankDetailRepository _bankDetailRepository;
+		private IUserRepository _userRepository;
+		private RepositoryContext _repositoryContext;
+
+		#endregion
+
+		#region Constructors
+
+		public BankService(
+            IBankDetailRepository bankDetailRepository,
+			IUserRepository userRepository,			
+			RepositoryContext repositoryContext)
+		{
+			_bankDetailRepository = bankDetailRepository;
+            _userRepository = userRepository;
+            _repositoryContext = repositoryContext;
+		}
+
+		#endregion
+
+		#region Methods
+
+		public async Task Create(BankDetail model, string userIdentifier)
+		{
+			var loggedInUser = await _userRepository.GetByUserNameWithDetails(userIdentifier);
+
+			model.CreatedUserId = loggedInUser.Id;
+			model.CreatedDateTime = DateTime.Now;
+			await _bankDetailRepository.CreateAsync(model);
+		}
+
+		public async Task Update(BankDetail model, string userIdentifier)
+		{
+			var loggedInUser = await _userRepository.GetByUserNameWithDetails(userIdentifier);
+
+			model.UpdatedUserId = loggedInUser.Id;
+			model.UpdatedDateTime = DateTime.Now;
+
+			var oldEntity = await this._repositoryContext.BankDetails.FindAsync(model.Id);
+			await _bankDetailRepository.UpdateAsync(oldEntity, model, true, loggedInUser.Id);
+		}
+	
+        public Task<IEnumerable<BankDetail>> GetBankDetailsById(int banDetailId)
+        {
+            throw new NotImplementedException();
+        }     
+
+        public Task<IEnumerable<BankDetail>> GetBankDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
