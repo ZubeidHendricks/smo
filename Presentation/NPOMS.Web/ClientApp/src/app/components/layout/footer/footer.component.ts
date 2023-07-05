087753e6d7533d67451c8db79d7285a8c7789d96
Kathiravan Murugan <Kathiravan.Murugan@westerncape.gov.za>
2023-07-05 17:18:53 +0200

    Version-Update

--- Presentation/NPOMS.Web/ClientApp/src/app/components/layout/footer/footer.component.ts
+++ Presentation/NPOMS.Web/ClientApp/src/app/components/layout/footer/footer.component.ts
@@ -27,10 +27,10 @@ export class FooterComponent implements OnInit {
   }
 
   currentVersion(): string {
-    return '2.1.0';
+    return '2.1.1';
   }
 
   deploymentDate(): string {
-    return '04 July 2023';
+    return '05 July 2023';
   }
 }
