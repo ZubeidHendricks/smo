5620cbabca43d53eab8566df33f51ad7eca0a108
admin <admin@DESKTOP-SIHE4MV>
2023-09-12 21:20:57 +0200

    status related issue fix for non compliance

--- Presentation/NPOMS.Web/ClientApp/src/app/components/layout/footer/footer.component.ts
+++ Presentation/NPOMS.Web/ClientApp/src/app/components/layout/footer/footer.component.ts
@@ -27,10 +27,10 @@ export class FooterComponent implements OnInit {
   }
 
   currentVersion(): string {
-    return '2.4.3';
+    return '2.4.5';
   }
 
   deploymentDate(): string {
-    return '11 Sep 2023';
+    return '12 Sep 2023';
   }
 }
