3c6f6ffcbd17a92a246e7aeddb21c637f3ce7069
admin <admin@DESKTOP-SIHE4MV>
2023-10-03 13:00:32 +0200

    update code

--- Presentation/NPOMS.Web/ClientApp/src/app/components/layout/footer/footer.component.ts
+++ Presentation/NPOMS.Web/ClientApp/src/app/components/layout/footer/footer.component.ts
@@ -27,10 +27,10 @@ export class FooterComponent implements OnInit {
   }
 
   currentVersion(): string {
-    return '2.5.2';
+    return '2.5.3';
   }
 
   deploymentDate(): string {
-    return '28 September 2023';
+    return '03 October 2023';
   }
 }
