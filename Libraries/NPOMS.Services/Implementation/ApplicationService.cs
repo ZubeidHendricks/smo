ae1e7bf482f8d20b9944ecebc22c69ca2239365b
admin <admin@DESKTOP-SIHE4MV>
2024-05-19 22:06:43 +0200

    Commented code removed

--- Libraries/NPOMS.Services/Implementation/ApplicationService.cs
+++ Libraries/NPOMS.Services/Implementation/ApplicationService.cs
@@ -154,14 +154,6 @@ namespace NPOMS.Services.Implementation
 
 				return results;
 			}
-			//else if (loggedInUser.Roles.Any(x => x.IsActive && x.RoleId.Equals((int)RoleEnum.Applicant)))
-   //         {
-   //             if (!loggedInUser.Departments[0].DepartmentId.Equals((int)DepartmentEnum.DoH))
-   //                 results = results.Where(x => x.ApplicationPeriod.DepartmentId.Equals(loggedInUser.Departments[0].DepartmentId));
-
-   //             return results;
-
-   //         }
 			else
 			{
 				// Get assigned organisations
