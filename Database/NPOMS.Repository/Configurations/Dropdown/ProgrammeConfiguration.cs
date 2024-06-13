7e0d5f7b0a23092fe0d98514a0092b29df7bcf66
admin <admin@DESKTOP-SIHE4MV>
2024-06-13 12:19:03 +0200

    Update code to local branch

--- Database/NPOMS.Repository/Configurations/Dropdown/ProgrammeConfiguration.cs
+++ Database/NPOMS.Repository/Configurations/Dropdown/ProgrammeConfiguration.cs
@@ -25,163 +25,184 @@ namespace NPOMS.Repository.Configurations.Dropdown
 					Id = 2,
 					Name = "Chronic Diseases and Non-Communicable Diseases",
 					Description = "Chronic Diseases and Non-Communicable Diseases",
-					DepartmentId = 11
-				},
+					DepartmentId = 11,
+                    Code = ""
+                },
 				new Programme
 				{
 					Id = 3,
 					Name = "HIV/AIDS, TB, STI",
 					Description = "HIV/AIDS, TB, STI",
-					DepartmentId = 11
-				},
+					DepartmentId = 11,
+                    Code = ""
+                },
 				new Programme
 				{
 					Id = 4,
 					Name = "Maternal, Women and Child Health",
 					Description = "Maternal, Women and Child Health",
-					DepartmentId = 11
-				},
+					DepartmentId = 11,
+                    Code = ""
+                },
 				new Programme
 				{
 					Id = 5,
 					Name = "Theatre, Surgery and Aneasthetics",
 					Description = "Theatre, Surgery and Aneasthetics",
-					DepartmentId = 11
-				},
+					DepartmentId = 11,
+                    Code = ""
+                },
 				new Programme
 				{
 					Id = 6,
 					Name = "Mental Health",
 					Description = "Mental Health",
-					DepartmentId = 11
-				},
+					DepartmentId = 11,
+                    Code = ""
+                },
 				new Programme
 				{
 					Id = 7,
 					Name = "Emergency Centre Presures",
 					Description = "Emergency Centre Presures",
-					DepartmentId = 11
-				},
+					DepartmentId = 11,
+                    Code = ""
+                },
 				new Programme
 				{
 					Id = 8,
 					Name = "Care and Support to Families",
 					Description = "Care and Support to Families",
 					DepartmentId = 7,
-					DirectorateId = 1
-				},
+					DirectorateId = 1,
+                    Code = ""
+                },
 				new Programme
 				{
 					Id = 9,
 					Name = "Child Care and Protection Services",
 					Description = "Child Care and Protection Services",
 					DepartmentId = 7,
-					DirectorateId = 1
-				},
+					DirectorateId = 1,
+                    Code = ""
+                },
 				new Programme
 				{
 					Id = 10,
 					Name = "Crime Prevention",
 					Description = "Crime Prevention",
 					DepartmentId = 7,
-					DirectorateId = 7
-				},
+					DirectorateId = 7,
+                    Code = ""
+                },
 				new Programme
 				{
 					Id = 11,
 					Name = "ECD & Partial Care",
 					Description = "ECD & Partial Care",
 					DepartmentId = 7,
-					DirectorateId = 3
-				},
+					DirectorateId = 3,
+                    Code = ""
+                },
 				new Programme
 				{
 					Id = 12,
 					Name = "EPWP",
 					Description = "EPWP",
 					DepartmentId = 7,
-					DirectorateId = 2
-				},
+					DirectorateId = 2,
+                    Code = ""
+                },
 				new Programme
 				{
 					Id = 13,
 					Name = "Facility Managment",
 					Description = "Facility Managment",
 					DepartmentId = 7,
-					DirectorateId = 4
-				},
+					DirectorateId = 4,
+                    Code = ""
+                },
 				new Programme
 				{
 					Id = 14,
 					Name = "Institutional Capacity Building",
 					Description = "Institutional Capacity Building",
 					DepartmentId = 7,
-					DirectorateId = 5
-				},
+					DirectorateId = 5,
+                    Code = ""
+                },
 				new Programme
 				{
 					Id = 15,
 					Name = "Care and Services to Older Persons",
 					Description = "Care and Services to Older Persons",
 					DepartmentId = 7,
-					DirectorateId = 8
-				},
+					DirectorateId = 8,
+                    Code = ""
+                },
 				new Programme
 				{
 					Id = 16,
 					Name = "Services to persons with Disabilities",
 					Description = "Services to persons with Disabilities",
 					DepartmentId = 7,
-					DirectorateId = 8
-				},
+					DirectorateId = 8,
+                    Code = ""
+                },
 				new Programme
 				{
 					Id = 17,
 					Name = "Substance Abuse",
 					Description = "Substance Abuse",
 					DepartmentId = 7,
-					DirectorateId = 8
-				},
+					DirectorateId = 8,
+                    Code = ""
+                },
 				new Programme
 				{
 					Id = 18,
 					Name = "Sustainable Livelihood",
 					Description = "Sustainable Livelihood",
 					DepartmentId = 7,
-					DirectorateId = 2
-				},
+					DirectorateId = 2,
+                    Code = ""
+                },
 				new Programme
 				{
 					Id = 19,
 					Name = "Victim Empowerment",
 					Description = "Victim Empowerment",
 					DepartmentId = 7,
-					DirectorateId = 7
-				},
+					DirectorateId = 7,
+                    Code = ""
+                },
 				new Programme
 				{
 					Id = 20,
 					Name = "Youth Development",
 					Description = "Youth Development",
 					DepartmentId = 7,
-					DirectorateId = 2
-				},
+					DirectorateId = 2,
+                    Code = ""
+                },
 				new Programme
 				{
 					Id = 21,
 					Name = "Child and Youth Care Centres",
 					Description = "Child and Youth Care Centres",
 					DepartmentId = 7,
-					DirectorateId = 4
-				},
+					DirectorateId = 4,
+                    Code = ""
+                },
 				new Programme
 				{
 					Id = 22,
 					Name = "ECD Conditional Grant",
 					Description = "ECD Conditional Grant",
 					DepartmentId = 7,
-					DirectorateId = 3
-				},
+					DirectorateId = 3,
+                    Code = ""
+                },
 				//new Programme
 				//{
 				//	Id = 23,
