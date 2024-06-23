f96c4446ca5f9747f07124f0a92b463ffb9fb7bb
admin <admin@DESKTOP-SIHE4MV>
2024-06-23 22:58:23 +0200

    Implemented Program, Sub Program and Sub program Type filter in budget summary page

--- Presentation/NPOMS.Web/ClientApp/src/app/components/layout/footer/footer.component.ts
+++ Presentation/NPOMS.Web/ClientApp/src/app/components/layout/footer/footer.component.ts
@@ -27,10 +27,10 @@ export class FooterComponent implements OnInit {
   }
 
   currentVersion(): string {
-    return '2.4.3';
+    return '2.4.4';
   }
 
   deploymentDate(): string {
-    return '19 June 2024';
+    return '23 June 2024';
   }
 }
