ddc4ee87d8476bfbbc2a10986be9e705ce2a463d
admin <admin@DESKTOP-SIHE4MV>
2024-08-22 12:36:07 +0200

    Dispaly application with new status for DSD

--- Presentation/NPOMS.Web/ClientApp/src/app/components/application/application-list/application-list.component.ts
+++ Presentation/NPOMS.Web/ClientApp/src/app/components/application/application-list/application-list.component.ts
@@ -297,13 +297,19 @@ export class ApplicationListComponent implements OnInit {
 
   private getSummarySubmissionStatus(application: IApplication, applicationId: number) {
 
-    this.capturedResponse = this.capturedResponses.filter(x => x.questionCategoryId === 100 && x.isActive === true && x.fundingApplicationId === applicationId);
-    if (this.capturedResponses.length > 0) {
-      application.submittedScorecard = this.capturedResponses.length
+    if(this.capturedResponses != undefined)
+    {
+      this.capturedResponse = this.capturedResponses.filter(x => x.questionCategoryId === 100 && x.isActive === true && x.fundingApplicationId === applicationId);
+      
+      if (this.capturedResponse.length > 0) {
+        application.submittedScorecard = this.capturedResponse.length
+      }
+     
     }
     else{
       application.submittedScorecard = 0;
-    }
+    }   
+   
   }
 
   private getAllCapturedResponses() {
