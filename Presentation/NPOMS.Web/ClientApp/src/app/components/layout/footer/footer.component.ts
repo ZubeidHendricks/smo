4636e8f6dfc281134348ec4478f8763d0139caa4
admin <admin@DESKTOP-SIHE4MV>
2024-09-05 09:38:03 +0200

    Quick Capture deployment in Prod

--- Presentation/NPOMS.Web/ClientApp/src/app/components/layout/footer/footer.component.ts
+++ Presentation/NPOMS.Web/ClientApp/src/app/components/layout/footer/footer.component.ts
@@ -27,10 +27,10 @@ export class FooterComponent implements OnInit {
   }
 
   currentVersion(): string {
-    return '2.7.7';
+    return '2.7.8';
   }
 
   deploymentDate(): string {
-    return '02 Sep 2024';
+    return '05 Sep 2024';
   }
 }
