d8a01361e652f0f442d0e25cff018c7aa379abcd
Tendai Mudadi <Tendai.Mudadi@westerncape.gov.za>
2024-09-06 14:13:33 +0200

    details of income and expenditure

--- Presentation/NPOMS.Web/ClientApp/src/app/npo-reporting-steps/details-of-income-and-and-expenditure-report/details-of-income-and-and-expenditure-report.component.ts
+++ Presentation/NPOMS.Web/ClientApp/src/app/npo-reporting-steps/details-of-income-and-and-expenditure-report/details-of-income-and-and-expenditure-report.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-details-of-income-and-and-expenditure-report',
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details-of-income-and-and-expenditure-report.component.css']
 })
 export class DetailsOfIncomeAndAndExpenditureReportComponent implements OnInit {
+  @Input() activeStep: number;
+  @Output() activeStepChange: EventEmitter<number> = new EventEmitter<number>();
 
+  data = [
+    { column1: ' ', column2: '', column3: '', column4: '' },
+    { column1: ' ', column2: '', column3: '', column4: '' },
+    { column1: ' ', column2: '', column3: '', column4: '' },
+    { column1: ' ', column2: '', column3: '', column4: '' },
+    { column1: ' ', column2: '', column3: '', column4: '' },
+  
+  ];
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  nextPage() {
+    this.activeStep = this.activeStep + 1;
+    this.activeStepChange.emit(this.activeStep);
+  }
+  
+
+  prevPage() {
+    this.activeStep = this.activeStep - 1;
+    this.activeStepChange.emit(this.activeStep);
+  }
+
 
 }
