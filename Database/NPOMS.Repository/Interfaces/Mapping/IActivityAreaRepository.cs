13278b9a85dfb4fc2e982fe0e03b61fc241d745d
Tendai Mudadi <Tendai.Mudadi@westerncape.gov.za>
2024-09-20 15:48:41 +0200

    ActivityAreaRepository

---
+++ Database/NPOMS.Repository/Interfaces/Mapping/IActivityAreaRepository.cs
@@ -0,0 +1,11 @@
+﻿using NPOMS.Domain.Mapping;
+
+namespace NPOMS.Repository.Interfaces.Mapping
+{
+    public interface IActivityAreaRepository : IBaseRepository<ActivityArea>
+    {
+        Task<ActivityArea> GetByModel(ActivityArea model);
+
+        Task<IEnumerable<ActivityArea>> GetByActivityId(int activityId, bool isActive);
+    }
+}
