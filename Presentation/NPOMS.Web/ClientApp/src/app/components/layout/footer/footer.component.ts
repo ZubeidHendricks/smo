e8ffd8dfbae74d161a9246debe32d83f747341fb
Tendai Mudadi <Tendai.Mudadi@westerncape.gov.za>
2024-10-08 12:23:41 +0200

    George

--- Presentation/NPOMS.Web/ClientApp/src/app/components/layout/footer/footer.component.ts
+++ Presentation/NPOMS.Web/ClientApp/src/app/components/layout/footer/footer.component.ts
@@ -27,10 +27,10 @@ export class FooterComponent implements OnInit {
   }
 
   currentVersion(): string {
-    return '2.8.3';
+    return '2.8.4';
   }
 
   deploymentDate(): string {
-    return '23 September 2024';
+    return '08 October 2024';
   }
 }
