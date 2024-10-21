964d2db71da4244709f1066ed898ee55172c1ba1
Tendai Mudadi <Tendai.Mudadi@westerncape.gov.za>
2024-10-21 11:00:44 +0200

    sdip report

---
+++ Database/NPOMS.Repository/Implementation/Entities/SDIPRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NPOMS.Domain.Entities;
+using NPOMS.Repository.Interfaces.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPOMS.Repository.Implementation.Entities
+{
+    public class SDIPRepository : BaseRepository<SDIPReport>, ISDIPRepository
+    {
+        public SDIPRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+        }
+
+        public async Task CreateEntity(SDIPReport model)
+        {
+            await CreateAsync(model);
+        }
+
+        public async Task<SDIPReport> GetById(int id)
+        {
+            return await FindByCondition(x => x.Id.Equals(id)).AsNoTracking().FirstOrDefaultAsync();
+        }
+
+        public async  Task<SDIPReport> GetByIds(int financialYearId, int applicationTypeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<SDIPReport>> GetByNpoId(int npoId)
+        {
+            return null;
+        }
+
+        public async Task<IEnumerable<SDIPReport>> GetByPeriodId(int applicationPeriodId)
+        {
+            return await FindByCondition(x => x.ApplicationId == applicationPeriodId && x.IsActive)
+                        .AsNoTracking()
+                        .ToListAsync();
+        }
+
+        public async Task<IEnumerable<SDIPReport>> GetEntities()
+        {
+            return await FindByCondition(x => x.IsActive).AsNoTracking().ToListAsync();
+        }
+
+        public async Task UpdateEntity(SDIPReport model, int currentUserId)
+        {
+            await UpdateAsync(null, model, false, currentUserId);
+        }
+    }
+}
---
+++ Libraries/NPOMS.Services/Implementation/SDIPService.cs
@@ -0,0 +1,73 @@
+﻿using NPOMS.Domain.Entities;
+using NPOMS.Repository.Implementation.Entities;
+using NPOMS.Repository.Interfaces.Core;
+using NPOMS.Repository.Interfaces.Entities;
+using NPOMS.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPOMS.Services.Implementation
+{
+    public class SDIPService : ISDIPService
+    {
+        private ISDIPRepository _sdipRepository;
+        private IUserRepository _userRepository;
+        public SDIPService(ISDIPRepository sdipRepository, IUserRepository userRepository)
+        {
+            _sdipRepository = sdipRepository;
+            _userRepository = userRepository;
+
+
+        }
+
+        public async Task CreateSDIPReportEntity(SDIPReport model, string userIdentifier)
+        {
+            var loggedInUser = await _userRepository.GetByUserNameWithDetails(userIdentifier);
+
+            model.CreatedUserId = loggedInUser.Id;
+            model.CreatedDateTime = DateTime.Now;
+
+            await _sdipRepository.CreateEntity(model);
+        }
+
+        public Task<SDIPReport> GetByIds(int financialYearId, int applicationTypeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<SDIPReport> GetSDIPReportById(int id)
+        {
+            return await _sdipRepository.GetById(id);
+        }
+
+        public async Task<IEnumerable<SDIPReport>> GetSDIPReportByNpoId(int npoId)
+        {
+            return await _sdipRepository.GetByNpoId(npoId);
+        }
+
+        public async Task<IEnumerable<SDIPReport>> GetSDIPReportByPeriodId(int applicationPeriodId)
+        {
+            return await _sdipRepository.GetByPeriodId(applicationPeriodId);
+        }
+
+        public async Task<IEnumerable<SDIPReport>> GetSDIPReports()
+        {
+            return await _sdipRepository.GetEntities();
+        }
+
+        public async Task UpdateSDIPReportEntity(SDIPReport model, string currentUserId)
+        {
+            var loggedInUser = await _userRepository.GetByUserNameWithDetails(currentUserId);
+
+            model.CreatedUserId = loggedInUser.Id;
+            model.CreatedDateTime = DateTime.Now;
+
+            await _sdipRepository.UpdateEntity(model, loggedInUser.Id);
+        }
+
+   
+    }
+}
